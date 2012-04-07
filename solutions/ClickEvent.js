b45892039082610f253c47b99379507f09fa2e01
Daniel Ma <drailskid@yahoo.com>
2012-04-07 15:03:40 -0700

    Little changes to satisfy JSLint

--- solutions/ClickEvent.js
+++ solutions/ClickEvent.js
@@ -1,16 +1,16 @@
 (function () {
   'use strict';
-  
+
   var el = document.getElementById("clickMe");
-  
-  function alertClicked (e) {
-    alert("You clicked " + e.target);
+
+  function alertClicked() {
+    alert("You clicked on me!");
   }
-  
+
   // Add listeners
   if (el.addEventListener) {
-    el.addEventListener('click', alertClicked, false); 
-  } else if (el.attachEvent)  {
+    el.addEventListener('click', alertClicked, false);
+  } else if (el.attachEvent) {
     el.attachEvent('onclick', alertClicked);
   }
 }());
